c55a19614dcb3f22cfc9946d1880ef3b7a97c4cb
Tammy Fina <finatammy@gmail.com>
2021-11-10 16:20:29 -0500

    highscore added to endGame function

--- assets/js/game.js
+++ assets/js/game.js
@@ -168,10 +168,28 @@ var startGame = function() {
 // function to end the entire game
 var endGame = function() {
     window.alert("The game has now ended.  Let's see how you did!");
+    
+    //check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    
+    if(highScore === null) {
+        highScore = 0;
+    }
+    
+    // if player has more money than the high score, player has new highscore
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name, playerInfo.name");
+
+        alert(playerInfo.name + " now has the highscore of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + "did not beat the high score of " + highScore + ".  Good luck next time");
+    }
 
     // if player is still alive, player wins!
     if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a health score of " + playerInfo.health + " and " + playerInfo.money + " coins!");
+        window.alert("Great job, you've survived the game! You now have a health score of " + playerInfo.health + " and " + playerInfo.money + " dollars");
     }
     else {
         window.alert("You've lost your robot in battle.");
