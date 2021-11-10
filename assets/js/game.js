9515fc290fc4d63d345422fa554328952fb9e0fd
Tammy Fina <finatammy@gmail.com>
2021-11-09 22:41:42 -0500

    handle null robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,7 +74,8 @@ var startGame = function() {
         if (playerInfo.health > 0) {
             // let player know what round they are in , remember that arrays start at 0 so it needs to have 1 added to it
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-            console.log( "Welcome to Robot Gladiators! Round " + (i + 1));
+            // debugger;
+            // console.log( "Welcome to Robot Gladiators! Round " + (i + 1));
             // pick new enemy to fight based on the index of the enemyNames array
             var pickedEnemyObj = enemyInfo[i];
 
@@ -171,8 +172,23 @@ var shop = function() {
 
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+// ***************************************
+// ADD LOOP HERE WITH PROMPT AND CONDITION
+// ***************************************
+while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+} 
+
+console.log("Your robot's name is " + name);
+return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money:10,
